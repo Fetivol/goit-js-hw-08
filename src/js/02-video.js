037424c590021b89c8dd66a43549f8eea2eda424
Fetivol <Fetisowova@gmail.com>
2023-06-30 12:02:06 +0200

    setCurrentTime for video
    setCurrentTime for video from localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const handleTimeUpdate = throttle(({ seconds }) => {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+player.on('timeupdate', handleTimeUpdate);
+
+const currentTimeStemp = localStorage.getItem('videoplayer-current-time');
+console.log(Number(currentTimeStemp));
+player
+  .setCurrentTime(Number(currentTimeStemp))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
