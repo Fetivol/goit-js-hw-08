8627563453df110f35dc2ae925f0864148a955e8
Fetivol <Fetisowova@gmail.com>
2023-07-01 21:51:53 +0200

    Update
    Update for task 2 with video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,26 +4,16 @@ import throttle from 'lodash/throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const handleTimeUpdate = throttle(({ seconds }) => {
+const handleTimeUpdate = throttle(({ duration, seconds }) => {
   console.log(seconds);
-  localStorage.setItem('videoplayer-current-time', seconds);
+  if (seconds === duration) {
+    localStorage.setItem('videoplayer-current-time', 0);
+  } else {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }
 }, 1000);
 player.on('timeupdate', handleTimeUpdate);
 
 const currentTimeStemp = localStorage.getItem('videoplayer-current-time');
 console.log(Number(currentTimeStemp));
-player
-  .setCurrentTime(Number(currentTimeStemp))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(Number(currentTimeStemp));
